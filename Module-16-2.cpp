8a3e9b1513fc331b28c0d8f133d6dc871b317bb1
Andrey Gorelov <shorkun1974@yandex.ru>
2023-05-25 17:32:37 +0300

    первый запуск

--- Module-16-2.cpp
+++ Module-16-2.cpp
@@ -9,37 +9,30 @@
 Вам потребуется функция StringToDouble — std::stod. Она принимает на вход строку, содержащую в себе число.*/
 
 #include <iostream>
-#pragma warning(disable : 4996)
+#include <string>
 
-//точность сравнения
-const float epsilon = 0.01f;
-//максимальнаяскорость машины
-const float maxSpeed = 150.0f;
 
 int main()
 {
-	//начальная скорость
-	float currentSpeed = 0.0f;
-	//изменение скорости
-	float deltaSpeed = 0.0f;
-	//строка для вывода скорости
-	char speedStr[4];
+	//целая часть числа
+	std::string integerPart = "";
+	//дробная часть числа
+	std::string fractionalPart = "";
+	
 
+	//ввод целой и дробной части в строковом виде
+	std::cout << "Enter the integer part of a number: ";
+	std::cin >> integerPart;
+	std::cout << "Enter the fractional part of a number: ";
+	std::cin >> fractionalPart;
 
-	//основной цикл продолжается пока скорость не вернётсяк 0 с заданной точностью
-	do {
-		std::sprintf(speedStr, "%.1f", currentSpeed);
-		std::cout << "Current vehicle speed: " << speedStr << std::endl;
-		std::cout << "True speed: " << currentSpeed << std::endl;
+	//в строковом виде склеиваем обе части по очереди, вставляя точку
+	std::string numberStr = integerPart + ".";
+	numberStr += fractionalPart;
 
-		//ввод изменения скорости
-		std::cout << "Enter the vehicle speed change: ";
-		std::cin >> deltaSpeed;
-		currentSpeed += deltaSpeed;
-		//машина не может разогнаться быстрее 150 км/ч
-		if ((currentSpeed - maxSpeed) > epsilon) {
-			currentSpeed = maxSpeed;
-		}
-	} while (currentSpeed > epsilon);
-	std::cout << "The trip is over!";
+	//конвертируем в число
+	double number = std::stod(numberStr);
+
+	//выводим число для проверки	
+	std::cout << "Full view of the number: " << number;
 }
